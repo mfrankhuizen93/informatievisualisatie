ba9c89c43c8ed645c501997e6609bc00ade4a884
Michelle Frankhuizen <michelle@frankhuizen.nl>
2018-06-14 08:54:18 +0200

    Fixed colors

--- bubblemap_city.py
+++ bubblemap_city.py
@@ -8,24 +8,21 @@ import time
 start_time = time.time()
 gltCities = ""
 
-def createBubblemap(df):
-    print("--- %s seconds --- Started creating bubbleMap " % (time.time() - start_time))
-
-    years = df['dt'].unique()
+def getDataPerYear(df, year):
     limits = [(41, 51), (31, 40), (21, 30), (11, 20), (1, 10), (-10, 0), (-20, -11), (-30, -21), (-40, -31), (-50, -41)]
     colors = ['rgb(165,0,38)','rgb(215,48,39)','rgb(244,109,67)','rgb(253,174,97)','rgb(254,224,144)','rgb(224,243,248)','rgb(171,217,233)','rgb(116,173,209)','rgb(69,117,180)','rgb(49,54,149)']
-    temp = []
+    data = []
 
     for i in range(len(limits)):
         lim = limits[i]
         df_sub = df[(df['AverageTemperature'] > lim[0]) & (df['AverageTemperature'] < lim[1])]
 
-        a = dict(
+        trace = dict(
             type = 'scattergeo',
             locationmode = 'world',
-            lon = df_sub[ (df_sub['dt'] == 2010)]['Longitude'],
-            lat = df_sub[ (df_sub['dt'] == 2010)]['Latitude'],
-            text = df_sub[ (df_sub['dt'] == 2010)]['City'] + '<br>Average temperature ' + (df_sub[ (df_sub['dt'] == 2010)]['AverageTemperature']).astype(str)+' degrees',
+            lon = df_sub[ (df_sub['dt'] == year)]['Longitude'],
+            lat = df_sub[ (df_sub['dt'] == year)]['Latitude'],
+            text = df_sub[ (df_sub['dt'] == year)]['City'] + '<br>Average temperature ' + (df_sub[ (df_sub['dt'] == year)]['AverageTemperature']).astype(str)+' degrees',
             hoverinfo = 'text',
             marker = dict(
                 size = 15,
@@ -34,10 +31,19 @@ def createBubblemap(df):
                 sizemode = 'area'
             ), 
 
-            name = '{0} - {1}'.format( lim[1], lim[0])
+            name = '{0} - {1}'.format(lim[1], lim[0])
         )
 
-        temp.append(a)
+        data.append(trace)
+
+    return data
+
+def createBubblemap(df):
+    print("--- %s seconds --- Started creating bubbleMap " % (time.time() - start_time))
+
+    year = 2000
+
+    data = getDataPerYear(df, year) 
 
     layout = dict(
             title = 'Titel hier',
@@ -45,22 +51,22 @@ def createBubblemap(df):
             geo = dict(
                 projection=dict( type='natural earth' ),
                 showland = True,
-                landcolor = 'rgb(255, 255, 255)',
+                landcolor = 'rgb(65, 174, 118)',
                 subunitwidth=1,
                 countrywidth=1,
                 subunitcolor="rgb(255, 255, 255)",
                 countrycolor="rgb(255, 255, 255)",
                 showocean= True,
-                oceancolor = 'lightgrey'
+                oceancolor = 'rgb(116, 169, 207)'
             ),
         )
 
-    fig = dict( data=temp, layout=layout )
+    fig = dict(data=data, layout=layout)
     plot(fig, filename='bubbleMap.html' )
 
 def getData():
     if os.path.isfile('data/df.csv'):
-        df = pd.read_csv('data/df.csv')
+        gltByCity = pd.read_csv('data/df.csv')
         print("--- %s seconds --- Getting optimized dataset " % (time.time() - start_time))
     else:
         # Reading the dataset and storing it
@@ -92,10 +98,9 @@ def getData():
         gltByCity['Latitude'] = np.where(gltByCity['Latitude'].str[-1:] =='S', '-' + gltByCity['Latitude'].str[:-1], gltByCity['Latitude'].str[:-1])
         gltByCity['Longitude'] = np.where(gltByCity['Longitude'].str[-1:] =='W', '-' + gltByCity['Longitude'].str[:-1], gltByCity['Longitude'].str[:-1])
 
-        df = gltByCity
-        df.to_csv('data/df.csv')
+        gltByCity.to_csv('data/df.csv')
 
-    createBubblemap(df)
+    createBubblemap(gltByCity)
 
 if __name__ == '__main__':
     getData()
