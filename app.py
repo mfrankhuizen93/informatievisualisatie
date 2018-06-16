5d0ead609671de8caef54bfb59c75f7b435f6515
Michelle Frankhuizen <michelle@frankhuizen.nl>
2018-06-16 15:09:38 +0200

    Updated the app.py

--- app.py
+++ app.py
@@ -11,26 +11,31 @@ import bubblemap as bm
 app = dash.Dash()
 
 data = bm.getData();
-fig = bm.getFigure(data, 2010)
+# fig = bm.getFigure(data, 2010)
 
 app.layout  = html.Div([
-    dcc.Graph(id='bubblemap'),
-    dcc.Slider(
-        id='bubblemap_slider',
-        min=1750,
-        max=2010,
-        step=5,
-        value=1750,
-        marks={
-            1750: {'label': '1750'},
-            1802: {'label': '1802'},
-            1854: {'label': '1854'},
-            1906: {'label': '1906'},
-            1958: {'label': '1958'},
-            2010: {'label': '2010'}
-        },
-        # updatemode='drag'
-    )
+
+    html.Div([
+        dcc.Graph(id='bubblemap'),
+
+        html.Div([
+            dcc.Slider(
+                id='bubblemap_slider',
+                min=1750,
+                max=2010,
+                step=5,
+                value=1750,
+                marks={
+                    1750: {'label': '1750'},
+                    1802: {'label': '1802'},
+                    1854: {'label': '1854'},
+                    1906: {'label': '1906'},
+                    1958: {'label': '1958'},
+                    2010: {'label': '2010'}
+                }
+            )
+        ], style={'margin-left': '120px', 'margin-right': '120px'}),
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
 ])
 
 @app.callback(
