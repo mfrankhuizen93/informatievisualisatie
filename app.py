b185905727660da88ddfa0cab98f4eac5399af2c
Michelle Frankhuizen <michelle@frankhuizen.nl>
2018-06-20 13:05:47 +0200

    Renamed file

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ import plotly.plotly as py
 import numpy as np
 import os.path
 import bubblemap as bm
+import line as ln
 
 app = dash.Dash()
 
-data = bm.getData();
+bubblemap_data = bm.getData();
+line_data = ln.getData();
+line_global_data = ln.getGlobalData();
 
 app.layout  = html.Div([
 
@@ -33,6 +36,18 @@ app.layout  = html.Div([
                     2010: {'label': '2010'}
                 }
             )
+            ], style={'margin-left': '120px', 'margin-right': '120px'}),
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+
+    html.Div([
+        dcc.Graph(id='line'),
+
+        html.Div([
+            dcc.Dropdown(
+            id='line_dropdown',
+            options=ln.getOptions(),
+            value='Global'
+        )
         ], style={'margin-left': '120px', 'margin-right': '120px'}),
     ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
 ])
@@ -41,7 +56,17 @@ app.layout  = html.Div([
     dash.dependencies.Output('bubblemap', 'figure'),
     [dash.dependencies.Input('bubblemap_slider', 'value')])
 def update_output(year): 
-    return bm.getFigure(data, year)
+    return bm.getFigure(bubblemap_data, year)
+
+@app.callback(
+        dash.dependencies.Output('line', 'figure'),
+        [dash.dependencies.Input('line_dropdown', 'value')])
+def update_output(country): 
+    if country != 'Global':
+        temp = line_data
+    else:
+        temp = line_global_data
+    return ln.getFigure(temp, country)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- line.py
+++ line.py
@@ -1,44 +1,135 @@
 from plotly.offline import init_notebook_mode, plot
 import plotly.graph_objs as go
 import pandas as pd
-import numpy as np
+from numpy import arange,array,ones
 import os.path
 import time
+from scipy import stats
 
 start_time = time.time()
 
+
+def getOptions():
+    options = []
+
+    df = pd.read_csv('data/GlobalLandTemperaturesByCountry.csv')
+    countries = list(df['Country'].unique())
+    countries.append('Global')
+
+    for country in countries:
+        option = {'label': country, 'value': country}
+        options.append(option)
+
+    return options
+
+def getDataByDateRange(df, country, start = 1750, end = 2015):
+    # Create a trace
+    data = []
+
+    if country != 'Global':
+        y = df[(df['Country'] == country) & (df['dt'] > start) & (df['dt'] <= end)]['AverageTemperature']
+        x = df[(df['Country'] == country) & (df['dt'] > start) & (df['dt'] <= end)]['dt']
+        xi = arange(0, len(x))
+    else:
+        print(list(df))
+        y = df['LandAverageTemperature']
+        x = df['dt']
+        xi = arange(0, len(x))
+        
+    # Generated linear fit
+    slope, intercept, _, _, _ = stats.linregress(xi, y)
+
+    line = slope*xi+intercept
+
+    trace1 = go.Scatter(
+                      x=x,
+                      y=y,
+                      mode='markers',
+                      marker=go.Marker(color='rgb(255, 127, 14)'),
+                      name='Data'
+                      )
+
+    trace2 = go.Scatter(
+                      x=x,
+                      y=line,
+                      mode='lines',
+                      marker=go.Marker(color='rgb(31, 119, 180)'),
+                      name='Fit'
+                      )
+
+    data.append(trace1)
+    data.append(trace2)
+
+    return data
+
+def getFigure(data, country = 'Netherlands', start = 1750, end = 2015):
+    data = getDataByDateRange(data, country, start, end)
+    layout = dict(title = country + ' Average Temperature between ' + str(start) + ' - ' + str(end),
+                  xaxis = dict(title = 'Year -->'),
+                  yaxis = dict(title = 'Temperature in °C -->'),
+                )
+
+    return dict(data = data, layout = layout)
+
+def createScatter(data, country = 'Netherlands', start = 1750, end = 2015):
+    print("--- %s seconds --- Started creating GDP- and Temperaturegrowth by year " % (time.time() - start_time))
+
+    fig = getFigure(data, country)
+    plot(fig, filename='climate_change_scatter-and-line.html' )
+
+def getGlobalData():
+    if os.path.isfile('data/df_global2.csv'):
+        df = pd.read_csv('data/df_global.csv')
+    else:
+        df = pd.read_csv('data/GlobalTemperatures.csv')
+        df = df.dropna()
+        df['dt'] = pd.to_datetime(df['dt'], format='%Y-%m-%d')
+
+        df = df.groupby([pd.Grouper(key='dt', freq='1Y')]).mean().reset_index()
+
+        years = list(range(1750, 2014))
+        df = df[pd.DatetimeIndex(df['dt']).year.isin(years)]
+
+        # Remove month and day from full date 
+        df['dt'] = pd.DatetimeIndex(df['dt']).year
+        df.to_csv('data/df_global.csv')
+    return df
+
 def getData():
-    if os.path.isfile('data/df_ry.csv'):
-        df = pd.read_csv('data/df_line.csv')
-        print("--- %s seconds --- Getting optimized country dataset " % (time.time() - start_time))
+    if os.path.isfile('data/df_r.csv'):
+        df = pd.read_csv('data/df_gdp.csv')
+        df['dt'] = pd.to_datetime(df['dt'], format='%Y-%m-%d')
     else:
         # Reading the dataset and storing it
         df = pd.read_csv('data/GlobalLandTemperaturesByCountry.csv')
-        print("--- %s seconds --- Read dataset" % (time.time() - start_time))
 
         # Remove all empty elements
         df = df.dropna()
-        print("--- %s seconds --- Dropped empty rows" % (time.time() - start_time))
 
         # Cast string to datetime
         df['dt'] = pd.to_datetime(df['dt'], format='%Y-%m-%d')
-        print("--- %s seconds --- Converted dt to datetime" % (time.time() - start_time))
 
         # Merge all monthly items to one year item
-        df = df.groupby([pd.Grouper(key='dt', freq='5Y'), 'Country']).mean().reset_index()
-        print("--- %s seconds --- Grouped all by five years" % (time.time() - start_time))
+        df = df.groupby([pd.Grouper(key='dt', freq='1Y'), 'Country']).mean().reset_index()
 
         # Remove all dates not in scope
         years = list(range(1750, 2014))
         df = df[pd.DatetimeIndex(df['dt']).year.isin(years)]
-        print("--- %s seconds --- Removed all dates not in scope" % (time.time() - start_time))
         # Remove month and day from full date 
         df['dt'] = pd.DatetimeIndex(df['dt']).year
-        print("--- %s seconds --- Removed month and day from full date" % (time.time() - start_time))
-        df.to_csv('data/df_line.csv')
+        df.to_csv('data/df_gdp.csv')
 
     return df
 
 if __name__ == '__main__':
-	df = getData()
-	print(df.head(100))
+    country = 'Global'
+    start = 1900
+    end = 2015
+
+    if country != 'Global':
+        df = getData()
+    else:
+        df = getGlobalData()
+
+
+    createScatter(data, country, start=start, end=end)
