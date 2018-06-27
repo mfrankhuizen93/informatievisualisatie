975dd92785cddcbf3fb240a71b5df0fe681b457f
Michelle Frankhuizen <michelle@frankhuizen.nl>
2018-06-27 09:47:55 +0200

    Updated CO2 graph

--- co2.py
+++ co2.py
@@ -8,79 +8,51 @@ from scipy import stats
 
 start_time = time.time()
 
-def getGlobalDataByDateRange(df, co2, start = 1960, end = 2016):
-    maxAllowed = df[df['dt'] == 1850]['LandAverageTemperature'] + 2
-
-    y = df[(df['dt'] > start) & (df['dt'] <= end)]['LandAverageTemperature']
-    x = df[(df['dt'] > start) & (df['dt'] <= end)]['dt']
+def getGlobalDataByDateRange(co2, start = 1960, end = 2016):
+    x = co2[(co2['Year'] > start) & (co2['Year'] <= end)]['Year']
     
     y_co2 = co2[(co2['Year'] > start) & (co2['Year'] <= end)]['Global']
 
-    xi = arange(0, len(x))
-        
-    # Generated linear fit
-    slope, intercept, _, _, _ = stats.linregress(xi, y)
+    co2goal = float((co2[co2['Year'] == 1990]['Global']) / 2)
 
-    line = slope*xi+intercept
-
-
-    co2goal = co2[co2['Year'] == 1960]['Global']
-
-    trace1 = go.Scatter(
-                      x=x,
-                      y=y,
-                      mode='markers',
-                      marker=go.Marker(color='rgb(255, 204, 153)'),
-                      name='Temperature scatter'
-                      )
-
-    trace2 = go.Scatter(
-                      x=x,
-                      y=line,
-                      mode='lines',
-                      marker=go.Marker(color='rgb(255, 153, 51)'),
-                      name='Fitted line'
-                      )
+    print(co2goal)
 
     traceCO2 = go.Scatter(
                         x=x,
                         y=co2['Global'],
                         mode='lines',
                         marker=go.Marker(color='rgb(255, 153, 51)'),
-                        name='Fitted line'
-                        name='MtCO₂/10 emmission'
+                        name='CO₂ emmission'
                         )
 
-
-    print([co2goal[0]] * len(x))
     lineCO2 = go.Scatter(
                         x=x,
-                        y=[co2goal[0]] * len(x),
+                        y=[co2goal] * len(x),
                         line = dict(
                             color = ('rgb(205, 12, 24)'),
-                            width = 4,
+                            width = 2,
                             dash = 'dot'),
-                        name='MtCO₂/10 Goal'
+                        name='CO₂ Goal'
                         )
 
 
-    return [trace1, trace2, traceCO2, lineCO2]
+    return [traceCO2, lineCO2]
 
-def getFigure(df, co2, start = 1960, end = 2016):
+def getFigure(co2, start = 1960, end = 2016):
     print("--- %s seconds --- Getting figure of scatterplot with fitted line" % (time.time() - start_time))
 
-    data = getGlobalDataByDateRange(df, co2, start, end)
+    data = getGlobalDataByDateRange(co2, start, end)
     
-    layout = dict(title = 'Global - maximum, average and minimum temperature between ' + str(start) + ' - ' + str(end),
+    layout = dict(title = 'Global - average MtCO₂ emmission between ' + str(start) + ' - ' + str(end),
                   xaxis = dict(title = 'Year -->'),
-                  yaxis = dict(title = 'Temperature in °C -->'),
+                  yaxis = dict(title = 'Emmission in MtCO₂ -->'),
                 )
 
     return dict(data = data, layout = layout)
 
-def createScatter(df, co2, start = 1960, end = 2016):
+def createScatter(co2, start = 1960, end = 2016):
     print("--- %s seconds --- Plotting figure of scatterplot with fitted line" % (time.time() - start_time))
-    fig = getFigure(df, co2, start, end)
+    fig = getFigure(co2, start, end)
     plot(fig, filename='climate_change_scatter-and-line.html')
 
 def getGlobalData():
@@ -107,7 +79,6 @@ def getCO2Data():
     df = pd.read_csv('data/carbon/GlobalCarbonAtlas_territorial.csv', header=1)
     columns = list(df)[1:]
     df['Global'] = df[columns].mean(axis=1)
-    print(df.head())
     return df
 
 
@@ -117,6 +88,5 @@ if __name__ == '__main__':
     end = 2016
 
     co2 = getCO2Data()
-    df = getGlobalData()
 
-    createScatter(df, co2, start, end)
+    createScatter(co2, start, end)
